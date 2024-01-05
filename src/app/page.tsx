75d3e892f9a6bdb1d6e392bdb4a8049d97dc8f73
Alex Johnson <johnsonav1992@gmail.com>
2024-01-05 08:51:33 -0600

    workin noice

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,39 @@
-import { Button } from '@/components/ui/button'
+import { Button } from '@/components/ui/button';
+import { Card } from '@/components/ui/card';
 
-export default function Home() {
-  return (
-    <Button>Click Me</Button>
-  )
-}
+type Todo = {
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean;
+};
+
+const Home = async () => {
+    const res = await fetch( 'https://jsonplaceholder.typicode.com/todos/' );
+    const data: Todo[] = await res.json();
+
+    return (
+        <>
+            <Button>
+                Click Me
+            </Button>
+            <h1>Todos</h1>
+            <Card className='flex flex-wrap w-1/2 gap-2 justify-center p-5 m-4'>
+                {
+                    data?.map( todo => {
+                        return (
+                            <Card
+                                key={ todo.id }
+                                className='w-44 bg-slate-700 text-white p-2'
+                            >
+                                { todo.title }
+                            </Card>
+                        );
+                    } )
+                }
+            </Card>
+        </>
+    );
+};
+
+export default Home;
